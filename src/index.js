6892992feaa78c93be09549a8305457cd31014e5
Nathen Drees <nadrees@gmail.com>
2016-08-22 09:07:19 -0500

    Implemented constructor

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
-class WebpackDynamicsPlugin {
-    constructor() {
+import objectAssign from 'object-assign';
 
+class WebpackDynamicsPlugin {
+    constructor(options = {}) {
+        let defaultOptions = {
+            connectionString: null,
+            solutionName: ''
+        };
+
+        let mergedOptions = objectAssign({}, options);
+        mergedOptions.dynamics = objectAssign({}, defaultOptions, options.dynamics);
+        mergedOptions.files = options.files || [];
+
+        this.options = mergedOptions;
+
+        if (!this.options.dynamics.connectionString) {
+            throw new Error('dynamics.connectionString is required');
+        }
     }
 
     apply(compiler) {
--- src/index.spec.js
+++ src/index.spec.js
@@ -3,6 +3,21 @@ import {expect} from 'chai';
 import sinon from 'sinon';
 
 describe('The WebpackDynamicsPlugin', () => {
+    let args = null;
+
+    function createInstance() {
+        return new WebpackDynamicsPlugin(args);
+    }
+
+    beforeEach(() => {
+        args = {
+            dynamics: {
+                connectionString: 'test'
+            },
+            files: ['test/**/*.*']
+        };
+    });
+
     describe('prototype', () => {
         it('should have an apply function', () => {
             expect(WebpackDynamicsPlugin.prototype.apply).to.not.be.null;
@@ -16,7 +31,7 @@ describe('The WebpackDynamicsPlugin', () => {
             };
             sinon.spy(compiler, 'plugin');
 
-            let plugin = new WebpackDynamicsPlugin();
+            let plugin = createInstance();
 
             plugin.apply(compiler);
 
@@ -31,7 +46,45 @@ describe('The WebpackDynamicsPlugin', () => {
     });
 
     describe('constructor', () => {
-        //TODO
+        it('should handle the `dynamics` key missing', () => {
+            delete args.dynamics;
+
+            expect(createInstance).to.throw();
+        });
+
+        it('should validate that the connection string is required', () => {
+            args.dynamics.connectionString = null;
+
+            expect(createInstance).to.throw();
+        });
+
+        it('should default the solution name to empty string', () => {
+            let plugin = createInstance();
+
+            expect(plugin.options.dynamics.solutionName).to.eql('');
+        });
+
+        it('should not replace solution name if it was provided', () => {
+            args.dynamics.solutionName = 'testSolutionName';
+
+            let plugin = createInstance();
+
+            expect(plugin.options.dynamics.solutionName).to.eql(args.dynamics.solutionName);
+        });
+
+        it('should default the files key to an empty array', () => {
+            args.files = null;
+
+            let plugin = createInstance();
+
+            expect(plugin.options.files).to.eql([]);
+        });
+
+        it('should not override the files key if it was provided', () => {
+            let plugin = createInstance();
+
+            expect(plugin.options.files).to.eql(args.files);
+        });
     });
 
     describe('uploadFiles function', () => {
